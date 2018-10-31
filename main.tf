834743e39f474cc15e29d190dff2ddc86b9db3db
Joseph Wortmann <joseph.wortmann@gmail.com>
2018-10-31 09:56:52 -0500

    Updated to new referenced modules
    added outputs

--- main.tf
+++ main.tf
@@ -1,21 +1,21 @@
 module "log_bucket" {
   name_prefix = "${var.account_name}"
   source      = "QuiNovas/log-bucket/aws"
-  version     = "1.0.16"
+  version     = "2.0.0"
 }
 
 module "cloudtrail" {
   account_name  = "${var.account_name}"
   log_bucket    = "${module.log_bucket.id}"
   source        = "QuiNovas/cloudtrail/aws"
-  version       = "1.0.18"
+  version       = "2.0.0"
 }
 
 module "config" {
-  account_name  = "${var.account_name}"
-  log_bucket    = "${module.log_bucket.id}"
-  source        = "QuiNovas/config/aws"
-  version       = "1.0.16"
+  name        = "${var.account_name}"
+  log_bucket  = "${module.log_bucket.id}"
+  source      = "QuiNovas/config/aws"
+  version     = "2.1.0"
 }
 
 module "password_policy" {
--- outputs.tf
+++ outputs.tf
@@ -23,12 +23,47 @@ output "cloudtrail_sns_arn" {
   value       = "${module.cloudtrail.sns_arn}"
 }
 
-output "log_arn" {
+output "config_bucket_arn" {
+  description = "The bucket ARN that config writes output to."
+  value       = "${module.config.bucket_arn}"
+}
+
+output "config_bucket_id" {
+  description = "The bucket name that config writes output to."
+  value       = "${module.config.bucket}"
+}
+
+output "config_delivery_channel_id" {
+  description = "The name of the config delivery channel."
+  value       = "${module.config.delivery_channel_id}"
+}
+
+output "config_recorder_id" {
+  description = "Name of the config recorder."
+  value       = "${module.config.recorder_id}"
+}
+
+output "config_rule_arns" {
+  description = "The ARNs of the config rules."
+  value       = "${module.config.rule_arns}"
+}
+
+output "config_rule_ids" {
+  description = "The IDs of the config rules."
+  value       = "${module.config.rule_ids}"
+}
+
+output "config_topic_arn" {
+  description = "The ARN of the SNS topic AWS Config writes events to."
+  value       = "${module.config.topic_arn}"
+}
+
+output "log_bucket_arn" {
   description = "The ARN of the Log bucket. Will be of format arn:aws:s3:::bucketname."
   value       = "${module.log_bucket.arn}"
 }
 
-output "log_id" {
+output "log_bucket_id" {
   description = "The name of the Log bucket."
   value = "${module.log_bucket.id}"
 }
